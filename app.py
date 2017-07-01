9d2a0a9f86856c7282cfdfd8285d12207da3ea9f
Serhii <si@email.com>
2017-07-01 18:28:17 +0300

    Turned to simple Web-app

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+import signal
+from flask import Flask
+from buzz import generator
+
+app = Flask(__name__)
+
+signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
+
+@app.route("/")
+def generate_buzz():
+    page = '<html><body><h1>'
+    page += generator.generate_buzz()
+    page += '</h1></body></html>'
+    return page
+
+@app.route("/sergey")
+def show_sergey():
+    page = '<html><body><h1>'
+    page += 'Designed by Sergey'
+    page += '</h1></body></html>'
+    return page
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=os.getenv('PORT')) # port 5000 is the default
